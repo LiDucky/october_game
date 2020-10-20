adcf21ae51356f173730d9267c0b5047bc6d5dc7
NinjaDuck9000 <mvroom3@gmail.com>
2020-10-19 18:43:55 -0700

    Added collisions and fixed jumping

--- game_start.py
+++ game_start.py
@@ -9,8 +9,8 @@ pygame.init()
 pygame.display.set_caption('October Game') # change for actual title later
 
 # Window Size set
-WINDOW_HEIGHT = 480
-WINDOW_WIDTH = 720
+WINDOW_HEIGHT = 1080
+WINDOW_WIDTH = 1920
 screen = pygame.display.set_mode(( WINDOW_WIDTH, WINDOW_HEIGHT ))
 
 #initialize "Player" Class
@@ -25,43 +25,45 @@ while True: # game loop
             pygame.quit()
             sys.exit()
 
+    player.velocity_x = 0
     keys=pygame.key.get_pressed()
-    if keys[pygame.K_r]: # Player model reset
-        player.respawn()
     if keys[pygame.K_LEFT]:
-        player.x -= player.velocity_x
+        player.velocity_x = -5
     if keys[pygame.K_RIGHT]:
-        player.x += player.velocity_x
+        player.velocity_x = 5
     if player.grounded:
-        player.velocity_y = 0
         if keys[pygame.K_UP]:
-            player.velocity_y = -20
+            player.velocity_y = -25
             player.grounded = False
     else:
         player.velocity_y += gravity
         if player.velocity_y > max_velocity:
             player.velocity_y = max_velocity
 
-    player.y += player.velocity_y
-
     screen.fill((0,0,0))
     y = 0
-    for row in maps.map_four:
+    for row in maps.map_two:
         x = 0
         for tile in row:
             if tile == 1:
                 screen.blit(maps.tile_one, (x * maps.tile_size, y * maps.tile_size))
+            if tile == 2:
+                screen.blit(maps.tile_two, (x * maps.tile_size, y * maps.tile_size))
             if tile != 0:
                 solid_tiles.append(pygame.Rect(x * maps.tile_size, y * maps.tile_size, maps.tile_size, maps.tile_size))
             x += 1
         y += 1
+    
+    player.move(solid_tiles)
     screen.blit(player.image, (player.x, player.y))
     
+    if player.y > WINDOW_HEIGHT:
+        # add text for player death
+        text = pygame.font.Font(None, 20)
+        text_surface = text.render("You died. :( press r to try again.", True, [255,255,255], [0,0,0])
+        screen.blit(text_surface, (50, 50))
+        if keys[pygame.K_r]: # Player model reset
+            player.respawn()
+
     pygame.display.update()
     clock.tick(60) # run at 60fps
-    
-
-
-
-
-
--- player.py
+++ player.py
@@ -4,45 +4,42 @@ class Player():
     def __init__(self):
 
         self.image = pygame.image.load('Assets/Sprites/player.png')
+        self.image.convert()
         self.x = 300
-        self. y = 300
-        self.velocity_x = 5
+        self.y = 300
+        self.velocity_x = 0
         self.velocity_y = 0
-        self.grounded = True
+        self.grounded = False
         # self.max_jumps = 1
     
     def respawn(self):
         self.x = 300
         self.y = 300
 
-    def collision_test(self, rect, tiles):
+    def collision_test(self, tiles):
         hit_list = []
+        player_rect = self.image.get_rect(left=self.x, top=self.y)
         for tile in tiles:
-            if rect.colliderect(tile):
+            if player_rect.colliderect(tile):
                 hit_list.append(tile)
         return hit_list
 
-    def move(self, rect, movement, tiles):
-        collision_types = {
-            'top': False,
-            'bottom': False,
-            'right': False,
-            'left': False
-        }
+    def move(self, tiles):
+        self.x += self.velocity_x
+        hit_list = self.collision_test(tiles)
         for tile in hit_list:
-            if movement[0] > 0:
-                rect.right = tile.left
-                collision_types['right'] = True
-            elif movement[0] < 0:
-                rect.left = tile.right
-                collision_types['left'] = True    
-        rect.y += movement[1]
-        hit_list = collision_test(rect, tile)
+            if self.velocity_x > 0:
+                self.x = tile.left - int(self.image.get_width()) # collide on right side
+            elif self.velocity_x < 0:
+                self.x = tile.right # collide on left side
+        self.y += self.velocity_y
+        hit_list = self.collision_test(tiles)
         for tile in hit_list:
-            if movement[1] > 0:
-                rect.bottom = tile.top
-                collision_types['bottom'] = True
-            elif movement[0] < 0:
-                rect.top = tile.bottom
-                collision_types['top'] = True
-        return rect, collision_types
+            if self.velocity_y > 0:
+                self.y = tile.top - int(self.image.get_height()) # collide on bottom side
+                self.veolcity_y = 0
+                self.grounded = True
+            elif self.velocity_y < 0:
+                self.y = tile.bottom # collide on top side
+                self.velocity_y = 0
+        return self.image.get_rect
